cc7f3d0f337b358febb5aad4b6b8b23b2cce9ed3
Eric Cornelissen <ericornelissen@gmail.com>
2022-10-18 21:38:24 +0200

    Add Make command to validate licenses and run it in the CI
    Create a new Make command to run the `check-licenses.js` script after,
    if necessary, (re)generating the SBOM. Also add a continuous integration
    (CI) job that runs the license check (using the Make command).
    
    Temporarily disallows two licenses to demonstrate the behaviour of the
    script and CI job if a disallowed license is detected.

--- scripts/check-licenses.js
+++ scripts/check-licenses.js
@@ -12,8 +12,9 @@ const allowedLicenses = [
 	"0BSD",
 	"Apache-2.0", "Apache 2.0",
 	"Artistic-2.0",
-	"BSD-2-Clause",
-	"BSD-3-Clause",
+	// TODO: uncomment, this is only commented for demonstration purposes
+	//"BSD-2-Clause",
+	//"BSD-3-Clause",
 	"CC0-1.0",
 	"CC-BY-3.0",
 	"GPL-2.0-only",
