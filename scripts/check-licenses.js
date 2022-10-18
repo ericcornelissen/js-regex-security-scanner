d3041af6ab30ebcac6a49d5a7f9895defb3c9730
Eric Cornelissen <ericornelissen@gmail.com>
2022-10-18 21:05:43 +0200

    Create initial script to check licenses in the SBOM
    Write a script to evaluate the licenses found by Syft and put in the
    SBOM. If any "violations" (defined below) are found it exists with a
    non-zero exit code, first outputting all violations found. Else it exits
    with the exit code 0.
    
    A "violation" is defined as a project that does not have any license
    that's in the approved lists of licenses (which is defined at the top of
    the script). The current list is based purely on what licenses are
    currently in the project's SBOM. Some notable inclusions:
    
    - "BSD": not a standard license name - it appears in some sample code of
      an ISC licensed dependency of npm itself.
    - "Apache-2.0" and "Apache 2.0": the latter is not standard, but one
      dependency uses this spelling for the former license. I think it's
      safe to assume any dependency that uses the latter intents to former.
    - "OpenSSL" and "Zlib": project-specific licenses, based on my research
      of these licenses these are safe to allow.
    - "GPL-2.0-only" and "GPL-2.0-or-later": just the name "GPL" may invoke
      fear in some. For this project I believe using GPL-2.0'ed dependencies
      is fine because the project isn't distributed in binary form.
    
    This script is intended to evolve with the project's needs, this is
    really just an initial, quick, iteration.

---
+++ scripts/check-licenses.js
@@ -0,0 +1,113 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as process from "node:process";
+import * as url from "node:url";
+
+// ------------
+// License list
+// ------------
+
+const allowedLicenses = [
+	"BSD",
+	"0BSD",
+	"Apache-2.0", "Apache 2.0",
+	"Artistic-2.0",
+	"BSD-2-Clause",
+	"BSD-3-Clause",
+	"CC0-1.0",
+	"CC-BY-3.0",
+	"GPL-2.0-only",
+	"GPL-2.0-or-later",
+	"ISC",
+	"MIT",
+	"OpenSSL",
+	"Python-2.0",
+	"Zlib",
+];
+
+// ----------------
+// Helper functions
+// ----------------
+
+const matches = (string) => (regexp) => regexp.test(string);
+
+const quoted = (string) => `'${string}'`;
+
+const toMatchWholeWordExpression = (string) => {
+	const preWordExpr = /(?:^|[\s(])/.source;
+	const postWordExpr = /(?:[\s)]|$)/.source;
+	return new RegExp(`${preWordExpr}${string}${postWordExpr}`);
+};
+
+const isAllowedLicense = (license) =>
+	allowedLicenses.includes(license) ||
+	allowedLicenses
+		.map(toMatchWholeWordExpression)
+		.some(matches(license));
+
+// -------------
+// Load the SBOM
+// -------------
+
+const projectRoot = path.resolve(
+	path.dirname(
+		url.fileURLToPath(
+			new URL(import.meta.url),
+		),
+	),
+	"..",
+);
+const sbomFile = path.resolve(
+	projectRoot,
+	"sbom.json",
+);
+
+const rawSbom = fs.readFileSync(sbomFile);
+const sbom = JSON.parse(rawSbom);
+
+// -----------------
+// Evaluate licenses
+// -----------------
+
+const licenseViolations = sbom.artifacts
+	.filter(artifact => !artifact.licenses.some(isAllowedLicense))
+	.map(artifact => {
+		return {
+			licenses: artifact.licenses,
+			name: artifact.name,
+			type: artifact.type,
+		};
+	});
+
+// --------------
+// Output results
+// --------------
+
+if (licenseViolations.length > 0) {
+	licenseViolations.forEach(licenseViolation => {
+		const multipleLicenses = licenseViolation.licenses.length > 1;
+		console.log(
+			"The",
+			licenseViolation.type,
+			"dependency",
+			quoted(licenseViolation.name),
+			"is licensed under",
+			multipleLicenses
+				? licenseViolation.licenses.map(quoted).join(' and ')
+				: quoted(licenseViolation.licenses[0]),
+			"which",
+			multipleLicenses ? "are" : "is",
+			"not currently allowed",
+		);
+	});
+
+	console.log(/* newline */);
+	console.log(
+		licenseViolations.length,
+		"license violation(s) detected. Please review",
+	);
+
+	process.exit(1);
+} else {
+	console.log("No license violations detected");
+}
