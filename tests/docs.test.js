619ce8124581a85c3e928319c82990ef71be694b
Eric Cornelissen <ericornelissen@gmail.com>
2024-11-14 22:16:47 +0100

    Switch to the Node.js test runner
    Replace AVA in favor of the Node.js test runner to reduce the number of
    distinct dependencies^1 as well as try out the Node.js test runner's
    snapshot testing support. The switch is relatively easy, the majority of
    the changes are due to the addition of a top-level `test()` used for the
    grouping with the Node.js test runner (which, unlike AVA, doesn't group
    by test file) and differences in the snapshot format.
    
    --
    ^1: as well as cut down on deprecations in the dependency tree.

--- tests/docs.test.js
+++ tests/docs.test.js
@@ -4,63 +4,65 @@ import cp from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import process from "node:process";
+import test from "node:test";
 import url from "node:url";
-
-import test from "ava";
 
 const rootDir = path.resolve(
 	path.dirname(url.fileURLToPath(new URL(import.meta.url))),
 	"..",
 );
 
-for (const file of fs.readdirSync(rootDir)) {
-	if (path.extname(file) !== ".md") {
-		continue;
-	}
-
-	const content = fs.readFileSync(path.join(rootDir, file), {
-		encoding: "utf-8",
-	});
-
-	if (/<!--\s*doctest:ignore-file\s*-->/.test(content)) {
-		continue;
-	}
-
-	const expr =
-		/(?:<!--\s*doctest:([a-z-]+)\s*-->)?[\s\n\r]*```([a-z]+)\r?\n([^]+?)\r?\n\s*```/g;
-
-	let match;
-	let i = 0;
-	while ((match = expr.exec(content)) !== null) {
-		i++;
-
-		const directive = match[1];
-		if (directive !== undefined) {
-			switch (directive) {
-				case "ignore":
-					continue;
-				default:
-					throw new Error(`Unknown doctest directive: '${directive}'`);
-			}
-		}
-
-		const language = match[2];
-		const script = match[3]
-			.split(/\n/)
-			.map((line) => line.trim())
-			.join("\n")
-			.replace("docker", process.env.CONTAINER_ENGINE);
-		if (language !== "shell") {
+test("docs", async (t) => {
+	for (const file of fs.readdirSync(rootDir)) {
+		if (path.extname(file) !== ".md") {
 			continue;
 		}
 
-		test(`${file} snippet #${i}`, (t) => {
-			const { status } = cp.spawnSync(script, [], {
-				cwd: rootDir,
-				encoding: "utf-8",
-				shell: true,
-			});
-			t.snapshot(status);
+		const content = fs.readFileSync(path.join(rootDir, file), {
+			encoding: "utf-8",
 		});
+
+		if (/<!--\s*doctest:ignore-file\s*-->/.test(content)) {
+			continue;
+		}
+
+		const expr =
+			/(?:<!--\s*doctest:([a-z-]+)\s*-->)?[\s\n\r]*```([a-z]+)\r?\n([^]+?)\r?\n\s*```/g;
+
+		let match;
+		let i = 0;
+		while ((match = expr.exec(content)) !== null) {
+			i++;
+
+			const directive = match[1];
+			if (directive !== undefined) {
+				switch (directive) {
+					case "ignore":
+						continue;
+					default:
+						throw new Error(`Unknown doctest directive: '${directive}'`);
+				}
+			}
+
+			const language = match[2];
+			if (language !== "shell") {
+				continue;
+			}
+
+			const script = match[3]
+				.split(/\n/)
+				.map((line) => line.trim())
+				.join("\n")
+				.replace("docker", process.env.CONTAINER_ENGINE);
+
+			await t.test(`${file} snippet #${i}`, (t) => {
+				const { status } = cp.spawnSync(script, [], {
+					cwd: rootDir,
+					encoding: "utf-8",
+					shell: true,
+				});
+				t.assert.snapshot(status);
+			});
+		}
 	}
-}
+});
